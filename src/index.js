7f38ed3be191af826d61ef3aa279d88fe6c2141b
fronkin <feko992@gmail.com>
2023-02-25 18:21:26 +0300

    feat: add brackets

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 module.exports = function check(str, bracketsConfig) {
-  let bord = str.length;
+  let arr = str.length;
   bracketsConfig.forEach(a => str = str.split(a[0] + a[1]).join(''));
-  return str.length === 0 ? true : (str.length === bord ? false : check(str, bracketsConfig));
+  return str.length === 0 ? true : (str.length === arr ? false : check(str, bracketsConfig));
 }
