a499e490c815df3cd391530842d9a58b82131d85
fronkin <feko992@gmail.com>
2023-02-25 18:18:52 +0300

    feat: add brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let border = str.length;
+  bracketsConfig.forEach(a => str = str.split(a[0] + a[1]).join(''));
+  return str.length === 0 ? true : (str.length === border ? false : check(str, bracketsConfig));
 }
